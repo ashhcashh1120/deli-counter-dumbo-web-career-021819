e1693257b5a889d7c1b3bec551759b341edd4d4d
Ashley Archibald <aarchibald20@gmail.com>
2019-02-14 23:19:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,8 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  else message = "The line is currently:"
+  else
+     message = "The line is currently:"
 
     katz_deli.each_with_index do |value,index|
       message += "#{index.to_i+1}. #{value}"
